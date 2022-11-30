951f1712a014bc6173e5be14c5a2e9557367983f
FrancescaOz <francescaoz.pzz@gmail.com>
2022-11-30 17:00:27 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostAttiviComponent } from './post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './post-inattivi/post-inattivi.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Route[] = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "post",
+    component: PostComponent
+  },
+  {
+    path: "post/attivi",
+    component: PostAttiviComponent
+  },
+  {
+    path: "post/inattivi",
+    component: PostInattiviComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostAttiviComponent } from './post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './post-inattivi/post-inattivi.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostAttiviComponent,
+    PostInattiviComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
